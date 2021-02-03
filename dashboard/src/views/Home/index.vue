d722a725cdc4c5a58736dcbfc17f672b9361deb4
adilier <adlerdet@outlook.com>
2021-02-03 20:34:29 -0300

    first commit

---
+++ dashboard/src/views/Home/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <custom-header />
+  <contact />
+  <div class="flex justify-center py-10 bg-brand-gray">
+    <p class="font-medium text-center text-gray-800">
+      feedbacker &copy; 2021
+    </p>
+  </div>
+</template>
+
+<script>
+import Contact from './Contact.vue';
+import CustomHeader from './CustomHeader.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    CustomHeader,
+    Contact,
+  },
+  setup() {
+    function handleLogin() {}
+
+    function handleAccountCreate() {}
+  },
+};
+</script>
