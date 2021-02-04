f05099a036be73406f5cfd71ede5cfd3e096bfb3
adilier <adlerdet@outlook.com>
2021-02-03 21:11:33 -0300

    Create Custom Header

--- dashboard/src/router/index.js
+++ dashboard/src/router/index.js
@@ -1,40 +1,40 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router';
 
-const Home = () => import('@/views/Home')
-const Feedbacks = () => import('@/views/Feedbacks')
-const Credentials = () => import('@/views/Credentials')
+const Home = () => import('@/views/Home');
+const Feedbacks = () => import('@/views/Feedbacks');
+const Credentials = () => import('@/views/Credentials');
 
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
   },
   {
     path: '/feedbacks',
     name: 'Feedbacks',
     component: Feedbacks,
     meta: {
-      hasAuth: true
-    }
+      hasAuth: true,
+    },
   },
   {
     path: '/credentials',
     name: 'Credentials',
     component: Credentials,
     meta: {
-      hasAuth: true
-    }
+      hasAuth: true,
+    },
   },
   {
     path: '/:pathMatch(.*)*',
-    redirect: { name: 'Home' }
-  }
-]
+    redirect: { name: 'Home' },
+  },
+];
 
 const router = createRouter({
   history: createWebHistory('/'),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
--- dashboard/src/views/Home/index.vue
+++ dashboard/src/views/Home/index.vue
@@ -1,14 +1,14 @@
 <template>
-  <custom-header />
+  <custom-header @create-account="handleAccountCreate" @login="handleLogin" />
   <contact />
   <div class="flex justify-center py-10 bg-brand-gray">
-    <p class="font-medium text-center text-gray-800">
-      feedbacker &copy; 2021
-    </p>
+    <p class="font-medium text-center text-gray-800">feedbacker &copy; 2021</p>
   </div>
 </template>
 
 <script>
+import { onMounted } from 'vue';
+import { useRouter } from 'vue-router';
 import Contact from './Contact.vue';
 import CustomHeader from './CustomHeader.vue';
 
@@ -19,9 +19,24 @@ export default {
     Contact,
   },
   setup() {
+    const router = useRouter();
+
+    onMounted(() => {
+      const token = window.localStorage.getItem('token');
+
+      if (token) {
+        router.push({ name: 'Feedback' });
+      }
+    });
+
     function handleLogin() {}
 
     function handleAccountCreate() {}
+
+    return {
+      handleLogin,
+      handleAccountCreate,
+    };
   },
 };
 </script>
